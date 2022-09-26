c68c34f7f477e22a90255413348832e1eb21cd5e
bader1980jo <bader.salah@gmail.com>
2022-09-26 12:05:15 +0400

    initial

--- NZWalks/NZWalks.API/WeatherForecast.cs
+++ NZWalks/NZWalks.API/WeatherForecast.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace NZWalks.API
 {
     public class WeatherForecast
@@ -10,6 +8,6 @@ namespace NZWalks.API
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }
